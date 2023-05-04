f3a2e854deefcb9cb99b3b1024c7b116e48a615c
Merillax <artem-gusev-04@mail.ru>
2023-05-04 15:29:01 +0300

    version 1.0

--- app/src/main/java/com/example/medicladin/MainActivity2.java
+++ app/src/main/java/com/example/medicladin/MainActivity2.java
@@ -2,13 +2,30 @@ package com.example.medicladin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private TextView skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        skip = findViewById(R.id.skip);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+
+            public void toLogin() {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/medicladin/MainActivity3.java
+++ app/src/main/java/com/example/medicladin/MainActivity3.java
@@ -2,13 +2,31 @@ package com.example.medicladin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    private TextView skip2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        skip2 = findViewById(R.id.skip2);
+        skip2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+
+            public void toLogin() {
+                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
