548b0a751b4e5fc2caf2b9987f4053026b01ca59
Merillax <artem-gusev-04@mail.ru>
2023-05-17 11:12:05 +0300

    version 1.0

--- app/src/main/java/com/example/medicladin/MainActivity4.java
+++ app/src/main/java/com/example/medicladin/MainActivity4.java
@@ -2,13 +2,30 @@ package com.example.medicladin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
-
+    private TextView cansel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        cansel = findViewById(R.id.cansel);
+        cansel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+
+            public void toLogin() {
+                Intent intent = new Intent(MainActivity4.this, MainActivity5.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
